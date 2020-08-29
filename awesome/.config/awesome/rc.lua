10d7b5d7c96b09b7713cb28cba78030ba61a7b6a
bladrome <blackwhitedoggie@163.com>
2020-08-29 19:03:37 +0800

    volume control for awesomewm

--- awesome/.config/awesome/rc.lua
+++ awesome/.config/awesome/rc.lua
@@ -441,31 +441,31 @@ globalkeys = my_table.join(
     -- ALSA volume control
     awful.key({ altkey }, "Up",
         function ()
-            os.execute(string.format("amixer -q set %s 2%%+", beautiful.volume.channel))
+            os.execute(string.format("amixer -c PCH set Master 2%%+"))
             beautiful.volume.update()
         end,
         {description = "volume up", group = "hotkeys"}),
     awful.key({ altkey }, "Down",
         function ()
-            os.execute(string.format("amixer -q set %s 2%%-", beautiful.volume.channel))
+            os.execute(string.format("amixer -c PCH set Master 2%%-"))
             beautiful.volume.update()
         end,
         {description = "volume down", group = "hotkeys"}),
     awful.key({ altkey }, "m",
         function ()
-            os.execute(string.format("amixer -q set %s toggle", beautiful.volume.togglechannel or beautiful.volume.channel))
+            os.execute(string.format("amixer -c PCH set Master 0%%"))
             beautiful.volume.update()
         end,
         {description = "toggle mute", group = "hotkeys"}),
     awful.key({ altkey, "Control" }, "m",
         function ()
-            os.execute(string.format("amixer -q set %s 100%%", beautiful.volume.channel))
+            os.execute(string.format("amixer -c PCH set Master 100%%"))
             beautiful.volume.update()
         end,
         {description = "volume 100%", group = "hotkeys"}),
     awful.key({ altkey, "Control" }, "0",
         function ()
-            os.execute(string.format("amixer -q set %s 0%%", beautiful.volume.channel))
+            os.execute(string.format("amixer -c PCH set Master 0%%"))
             beautiful.volume.update()
         end,
         {description = "volume 0%", group = "hotkeys"}),
