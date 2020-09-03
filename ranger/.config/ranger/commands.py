acca185044d6e0b9678587a39aa8665e36fcb73a
bladrome <blackwhitedoggie@163.com>
2020-09-03 18:04:57 +0800

    fzf + lazygit

--- ranger/.config/ranger/commands.py
+++ ranger/.config/ranger/commands.py
@@ -331,3 +331,50 @@ class ag(Command):
         if flg[0] == '-' and flg[1] in 'flvgprw':
             cmd += ' ' + flg
         return ['{} {}'.format(cmd, p) for p in reversed(ag.patterns)]
+
+
+
+
+
+class fzf_select(Command):
+    """
+    :fzf_select
+
+    Find a file using fzf.
+
+    With a prefix argument select only directories.
+
+    See: https://github.com/junegunn/fzf
+    """
+    def execute(self):
+        import subprocess
+        import os.path
+        if self.quantifier:
+            # match only directories
+            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            -o -type d -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+        else:
+            # match files and directories
+            command="find -L . \( -path '*/\.*' -o -fstype 'dev' -o -fstype 'proc' \) -prune \
+            -o -print 2> /dev/null | sed 1d | cut -b3- | fzf +m"
+        fzf = self.fm.execute_command(command, universal_newlines=True, stdout=subprocess.PIPE)
+        stdout, stderr = fzf.communicate()
+        if fzf.returncode == 0:
+            fzf_file = os.path.abspath(stdout.rstrip('\n'))
+            if os.path.isdir(fzf_file):
+                self.fm.cd(fzf_file)
+            else:
+                self.fm.select_file(fzf_file)
+
+
+
+# from ranger wiki
+class lazygit(Command):
+    
+    def execute(self):
+        dirname = self.fm.thisfile.path
+        cmd = "lazygit -p {}".format(dirname)
+        self.fm.execute_command(cmd)
+
+    def tab(self, tabnum):
+        return self._tab_directory_content()
