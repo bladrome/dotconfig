3ba646e0b4220fe79ae39033127390f0563db2ff
bladrome <blackwhitedoggie@163.com>
2020-10-07 18:16:13 +0800

    update emacs font size

--- doomemacs/.config/doom/config.el
+++ doomemacs/.config/doom/config.el
@@ -19,9 +19,9 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "Mono" :size 16 :weight 'semi-light)
+(setq doom-font (font-spec :family "Mono" :size 18 :weight 'semi-light)
       doom-variable-pitch-font (font-spec :family "Fira Sans") ; inherits `doom-font''s :size
-      doom-unicode-font (font-spec :family "Input Mono Narrow" :size 12)
+      doom-unicode-font (font-spec :family "Input Mono Narrow" :size 18)
       doom-big-font (font-spec :family "Fira Code" :size 19))
 
 
