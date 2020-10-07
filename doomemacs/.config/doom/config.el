faaeb7f1ddbd9d0f717ecfa2d09febaff86e2040
bladrome <blackwhitedoggie@163.com>
2020-10-07 18:13:31 +0800

    doom font config and org dir custom

--- doomemacs/.config/doom/config.el
+++ doomemacs/.config/doom/config.el
@@ -19,8 +19,11 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "monospace" :size 18 :weight 'semi-light)
-      doom-variable-pitch-font (font-spec :family "sans" :size 13))
+(setq doom-font (font-spec :family "Mono" :size 16 :weight 'semi-light)
+      doom-variable-pitch-font (font-spec :family "Fira Sans") ; inherits `doom-font''s :size
+      doom-unicode-font (font-spec :family "Input Mono Narrow" :size 12)
+      doom-big-font (font-spec :family "Fira Code" :size 19))
+
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
--- doomemacs/.config/doom/custom.el
+++ doomemacs/.config/doom/custom.el
@@ -4,7 +4,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files
-   '("~/Documents/2020" "~/org/journal.org" "~/org/notebook.org" "~/org/self.org" "~/org/todo.org" "~/org/work.org")))
+   '("~/Documents/2020" "~/Documents/2020/books" "~/org/journal.org" "~/org/notebook.org" "~/org/self.org" "~/org/todo.org" "~/org/work.org")))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
