3b43db51e40f02e8e5d3964dca31e034ac0448c8
bladrome <blackwhitedoggie@163.com>
2020-10-08 09:19:08 +0800

    emacs transparent, htop rc

--- doomemacs/.config/doom/config.el
+++ doomemacs/.config/doom/config.el
@@ -57,3 +57,6 @@
 ;; they are implemented.
 ;; (setq debug-on-quit t)
 ;; (setq debug-on-error t)
+;; set transparency
+(set-frame-parameter (selected-frame) 'alpha '(90 85))
+(add-to-list 'default-frame-alist '(alpha 90 85))
