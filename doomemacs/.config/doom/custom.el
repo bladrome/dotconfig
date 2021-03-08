dda9f930ed09700b6dc323469b24ebf4a74d6496
bladrome <blackwhitedoggie@163.com>
2021-03-08 21:56:34 +0800

    attachments at org dir

--- doomemacs/.config/doom/custom.el
+++ doomemacs/.config/doom/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-attach-id-dir "~/Documents/2021/attatchments")
  '(bongo-enabled-backends '(mpv))
  '(custom-safe-themes
    '("2f1518e906a8b60fac943d02ad415f1d8b3933a5a7f75e307e6e9a26ef5bf570" default))
