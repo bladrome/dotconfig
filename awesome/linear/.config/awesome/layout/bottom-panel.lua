f6979364f7f5ce60c05a694a585f31764fb52d6f
bladrome <blackwhitedoggie@163.com>
2021-03-12 14:50:06 +0800

    systray visible default

--- awesome/linear/.config/awesome/layout/bottom-panel.lua
+++ awesome/linear/.config/awesome/layout/bottom-panel.lua
@@ -62,7 +62,7 @@ local bottom_panel = function(s)
 			screen = 'primary',
 			widget = wibox.widget.systray
 		},
-		visible = false,
+		visible = true,
 		top = dpi(10),
 		widget = wibox.container.margin
 	}
