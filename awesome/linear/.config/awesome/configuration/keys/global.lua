9a5070070ac2871bf75311c475f96438448ea887
bladrome <blackwhitedoggie@163.com>
2021-10-12 18:07:59 +0800

    keep useless_gap during layout change

--- awesome/linear/.config/awesome/configuration/keys/global.lua
+++ awesome/linear/.config/awesome/configuration/keys/global.lua
@@ -113,7 +113,8 @@ local global_keys = awful.util.table.join(
 		{modkey},
 		'o',
 		function()
-			awful.tag.incgap(5)
+			awful.tag.incgap(5);
+			beautiful.useless_gap =  beautiful.useless_gap + 5
 		end,
 		{description = 'increase gap', group = 'layout'}
 	),
@@ -122,6 +123,7 @@ local global_keys = awful.util.table.join(
 		'o',
 		function()
 			awful.tag.incgap(-5)
+			beautiful.useless_gap =  beautiful.useless_gap - 5
 		end,
 		{description = 'decrease gap', group = 'layout'}
 	),
