c3edf05c589073b0dda7cc0118e85c42637c946b
bladrome <blackwhitedoggie@163.com>
2021-11-08 11:46:46 +0800

    add pkgs in ipadish

--- ipadish.sh
+++ ipadish.sh
@@ -3,7 +3,19 @@
 sed -i 's/dl-cdn.alpinelinux.org/mirrors.bfsu.edu.cn/g' /etc/apk/repositories
 apk update
 apk upgrade
-apk add zsh git neofetch curl neovim tmux openssh python3 rsync
+
+
+
+apk add zsh neofetch tmux python3 rsync grep awk sed
+
+apk add neovim highlight highlight-doc
+
+apk add build-base clang clang-doc nasm nasm-doc
+
+apk add man man-pages mdocml-apropos less less-doc
+
+apk add curl curl-dev curl-doc openssh openssh-doc git git-doc
+
 
 # openssh
 apk add openssh
