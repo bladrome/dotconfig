646525025524427ddbd0cd595e7945306307ec40
bladrome <blackwhitedoggie@163.com>
2022-01-15 20:32:19 +0800

    local gen

--- archpackages/install.sh
+++ archpackages/install.sh
@@ -55,7 +55,7 @@ echo 'en_US.UTF-8 UTF-8
 zh_CN.GB18030 GB18030
 zh_CN.GBK GBK
 zh_CN.UTF-8 UTF-8
-zh_CN GB2312' > /mnt/etc/locale.gen
+zh_CN GB2312' >> /mnt/etc/locale.gen
 arch-chroot /mnt /bin/bash -c locale-gen
 clear
 
