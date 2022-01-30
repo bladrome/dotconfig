04fc98a728d965979cddb1dc5f8a9fd39a0c42c6
blkcat <blackwhitedoggie@163.com>
2022-01-30 15:44:16 +0800

    Update install.sh

--- archpackages/install.sh
+++ archpackages/install.sh
@@ -60,7 +60,7 @@ if [ $EFI -eq 0 ]
 then
     chrootrun "grub-install --target=i386-pc ${DISCO}"
 else
-    chrootrun "grub-install --target=x86_64-efi --efi-directory=/mnt/boot"
+    chrootrun "grub-install --target=x86_64-efi --efi-directory=/mnt/boot --bootloader-id=GRUB"
 fi
 chrootrun "grub-mkconfig -o /boot/grub/grub.cfg"
 
