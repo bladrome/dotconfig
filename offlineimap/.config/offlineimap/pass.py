dde93cd6a8928557ffd5dd708b517b06bf702083
bladrome <blackwhitedoggie@163.com>
2022-04-22 17:15:17 +0800

    update offlineimap

--- offlineimap/.config/offlineimap/pass.py
+++ offlineimap/.config/offlineimap/pass.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 import os
 
-def get_pass():
-    return os.popen(
-        "gpg -q --no-tty -d ~/.config/.mailauthinfo.gpg"
-    ).read().strip()
+def get_pass(key):
+    text = os.popen(
+        "gpg -q --for-your-eyes-only --no-tty -d ~/.config/.mailauthinfo.gpg"
+    ).read().strip().split("\n")
+    text = [i.split() for i in text]
+    password = {i[0]: i[1] for i in text}
+    return password[key]
