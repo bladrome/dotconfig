2aac8f5b0cef88684081e6a9e3041facbc24276b
bladrome <blackwhitedoggie@163.com>
2022-07-12 18:35:25 +0800

    update wezterm font config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,5 +1,9 @@
+
 local wezterm = require 'wezterm';
+
 return {
-  color_scheme = "3024 (dark) (terminal.sexy)",
-  enable_tab_bar = false,
+  color_scheme = "Argonaut",
+  hide_tab_bar_if_only_one_tab = true,
+  warn_about_missing_glyphs = false,
+  font_size = 14,
 }
