1a5255519f26f5b322abc682dce50e9ecfebd6e9
bladrome <blackwhitedoggie@163.com>
2022-07-27 11:45:11 +0800

    update wezterm

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -2,8 +2,11 @@
 local wezterm = require 'wezterm';
 
 return {
+  font = wezterm.font("Fira Code"),
+  font_size = 14,
   color_scheme = "Argonaut",
   hide_tab_bar_if_only_one_tab = true,
   warn_about_missing_glyphs = false,
-  font_size = 14,
+  audible_bell  = "Disabled",
+  window_close_confirmation = "NeverPrompt",
 }
