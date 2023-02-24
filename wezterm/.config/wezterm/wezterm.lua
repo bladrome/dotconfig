c1a172ae500d8ed2c7639eae8cb0913a735a3f44
bladrome <blackwhitedoggie@163.com>
2023-02-24 10:10:09 +0800

    Change wezterm theme

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -5,7 +5,6 @@ return {
   font = wezterm.font("Fira Code"),
   font_size = 12,
   color_scheme = "Argonaut",
-  -- color_scheme = "Mathias",
   hide_tab_bar_if_only_one_tab = true,
   warn_about_missing_glyphs = false,
   audible_bell  = "Disabled",
