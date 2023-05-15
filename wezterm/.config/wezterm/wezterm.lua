6a775061fe051c400867ab1b591bdb78cd285ec7
bladrome <blackwhitedoggie@163.com>
2023-05-15 10:22:26 +0800

    change font size

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require 'wezterm';
 
 return {
   font = wezterm.font("Fira Code"),
-  font_size = 12,
+  font_size = 14,
   color_scheme = "Argonaut",
   hide_tab_bar_if_only_one_tab = true,
   warn_about_missing_glyphs = false,
